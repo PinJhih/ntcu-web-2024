e91efcc4c4aa0f986e5ca0f8d7602c68549953e8
PinJhih <isaacwang29@gmail.com>
2024-05-23 10:33:55 +0800

    fix: Raise error if form is empty

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var httpError = require("http-errors");
 
 var records = require("../models/records");
 
@@ -7,9 +8,12 @@ router.get("/", function (_, res) {
     res.render("index", { title: "NTCU Web Programming" });
 });
 
-router.all("/action", async function (req, res) {
+router.all("/action", async function (req, res, next) {
     let id = req.query.stu_id;
     let name = req.query.name;
+    if (id == undefined || name == undefined)
+        return next(httpError(400, "Error: Data Cannot be Empty"));
+
     records.addRecord(id, name);
     res.render("submit", { name: name })
 })
